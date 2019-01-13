cf9e378d24611e7563588da2008533ee4c4e7ccf
Brian Lovin <briandlovin@gmail.com>
2019-01-13 12:31:51 -0800

    SEO, metatags, og image, favicon

--- pages/_app.js
+++ pages/_app.js
@@ -44,7 +44,8 @@ class SecurityChecklistApp extends App {
         <GlobalStyles />
 
         <Head>
-          <meta name="theme-color" content="#16171A" key="theme-color" />
+          <meta name="theme-color" content="#FFF" key="theme-color" />
+
           <meta
             name="description"
             content="A checklist for staying safe on the internet"
@@ -52,15 +53,76 @@ class SecurityChecklistApp extends App {
 
           <link
             rel="apple-touch-icon"
-            sizes="180x180"
-            href="static/meta/apple-touch-icon.png"
+            sizes="57x57"
+            href="/static/apple-icon-57x57.png"
           />
-          <link rel="manifest" href="/static/meta/site.webmanifest" />
           <link
-            rel="mask-icon"
-            href="/static/meta/safari-pinned-tab.svg"
-            color="#16171A"
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/static/apple-icon-60x60.png"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/static/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/static/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/static/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/static/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/static/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/static/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/manifest.json" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <link rel="mask-icon" href="/static/pinned-tab.svg" color="#FFF" />
           <meta name="msapplication-TileColor" content="#ffffff" />
 
           <script
