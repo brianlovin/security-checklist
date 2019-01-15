2e59f0cbe600222b16261248bd550fe678c6b2ee
Kirill Mesnyankin <sadbox.games@gmail.com>
2019-01-15 21:37:34 +0300

    Add Geotagging check

---
+++ config/geotagging.js
@@ -0,0 +1,36 @@
+export default {
+  id: 'geotagging',
+  title: 'Turn OFF Geotagging for your media',
+  description: `Geotagging is the process of adding geographical identification to a media files (photos, videos, etc.). Anyone who has access to your media files (for example, in Instagram or Twitter) can read this data and find out information that you wouldn’t want to disclose.
+
+  \n\nEnsure that your camera does not tag your photos and videos with the current Geo Position.
+  `,
+  resources: [
+    {
+      name: 'Geotagging',
+      url: 'https://en.wikipedia.org/wiki/Geotagging',
+    },
+    {
+      name: 'What is Geotag Security and How it Helps to Protect Your Family',
+      url: 'https://www.geotag-security.com/',
+    },
+    {
+      name: 'Web Photos That Reveal Secrets, Like Where You Live',
+      url:
+        'https://www.nytimes.com/2010/08/12/technology/personaltech/12basics.html',
+    },
+    {
+      name: 'How to Avoid the Potential Risks of Geotagging',
+      url: 'https://www.wikihow.com/Avoid-the-Potential-Risks-of-Geotagging',
+    },
+    {
+      name: 'How to Turn OFF Geotagging For Photos On iPhone and iPad',
+      url:
+        'https://www.techbout.com/turn-off-geotagging-for-photos-iphone-ipad-8738/',
+    },
+    {
+      name: 'How To Turn Off Geotagging On Android Devices',
+      url: 'https://www.technobezz.com/turn-off-geotagging-android-devices/',
+    },
+  ],
+};
