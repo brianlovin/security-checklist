f75c2fdd82b7535de67891f184d3c5da7eb1c91e
Marc Stalfoort <marc.stalfoort@vodafoneziggo.com>
2019-01-16 23:12:23 +0100

    feat: add startpage.com search engine

--- config/searchEngine.js
+++ config/searchEngine.js
@@ -1,7 +1,7 @@
 export default {
   id: 'searchEngine',
   title: 'Use a privacy-first search engine',
-  description: `You should use a search engine that protects you from tracking, fingerprinting, and unwanted advertisements. DuckDuckGo is a privacy-first search engine that does not store your search history, has strict location and personalization permissions, and publishes regular content teaching people how to be safer on the web.
+  description: `You should use a search engine that protects you from tracking, fingerprinting, and unwanted advertisements. DuckDuckGo and Startpage.com are privacy-first search engines that do not store your search history, have strict location and personalization permissions, and DuckDuckGo publishes regular content teaching people how to be safer on the web.
     `,
   apps: [
     {
@@ -18,6 +18,20 @@ export default {
         linux: 'https://duckduckgo.com',
       },
     },
+    {
+      name: 'Startpage.com',
+      image: '/static/img/startpage.png',
+      url: 'https://www.startpage.com/',
+      sources: {
+        windows: 'https://www.startpage.com/',
+        macos: 'https://www.startpage.com/',
+        ios:
+          'https://www.startpage.com/en/search/download-startpage-plugin.html',
+        android:
+          'https://www.startpage.com/en/search/download-startpage-plugin.html',
+        linux: 'https://www.startpage.com/',
+      },
+    },
   ],
   resources: [
     {
@@ -32,6 +46,14 @@ export default {
       name: 'DuckDuckGo: No, we’re not using fingerprinting to track you',
       url:
         'https://techcrunch.com/2019/01/07/duckduckgo-browser-fingerprinting/',
+    },
+    {
+      name: 'Startpage.com privacy policy',
+      url: 'https://www.startpage.com/gb/search/privacy-policy.html',
+    },
+    {
+      name: 'About Startpage.com',
+      url: 'https://www.startpage.com/gb/about-us.html',
     },
     {
       name: 'Everything Google knows about you',
