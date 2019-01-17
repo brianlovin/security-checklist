69f4798ce1d267e9a111d60cb250b1785ad7ef6b
Brian Lovin <briandlovin@gmail.com>
2019-01-16 17:49:02 -0800

    Add truncation to only show first 3 apps by default

--- components/ChecklistItem/Apps.js
+++ components/ChecklistItem/Apps.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
-import { AppsContainer, SectionHeading } from './style';
+import { AppsContainer, SectionHeading, ExpandContainer } from './style';
+import { Button } from '../Button';
 import type { ChecklistResource } from '../../types';
 import { AppRow } from './App';
 
@@ -8,14 +9,45 @@ type Props = {
   resource: ChecklistResource,
 };
 
-export const Apps = ({ resource }: Props) => {
-  if (!resource.apps) return null;
-  return (
-    <AppsContainer>
-      <SectionHeading>Apps</SectionHeading>
-      {resource.apps.map(app => (
-        <AppRow key={app.name} app={app} />
-      ))}
-    </AppsContainer>
-  );
+type State = {
+  overflowExpanded: boolean,
 };
+
+export class Apps extends React.Component<Props, State> {
+  state = { overflowExpanded: false };
+
+  expand = () => this.setState({ overflowExpanded: true });
+
+  render() {
+    const { resource } = this.props;
+    const { overflowExpanded } = this.state;
+
+    if (!resource.apps) return null;
+
+    let appList = resource.apps;
+    let overflowAppList;
+    if (appList.length > 3) {
+      overflowAppList = appList.slice(3, appList.length);
+      appList = appList.slice(0, 3);
+    }
+
+    return (
+      <AppsContainer overflowExpanded={overflowExpanded}>
+        <SectionHeading>Apps</SectionHeading>
+        {appList.map(app => (
+          <AppRow key={app.name} app={app} />
+        ))}
+
+        {overflowAppList && !overflowExpanded && (
+          <ExpandContainer onClick={this.expand}>
+            <Button>Show more choices</Button>
+          </ExpandContainer>
+        )}
+
+        {overflowAppList &&
+          overflowExpanded &&
+          overflowAppList.map(app => <AppRow key={app.name} app={app} />)}
+      </AppsContainer>
+    );
+  }
+}
