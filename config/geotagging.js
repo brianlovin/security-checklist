9cc2ecf2465e2c1ca1d4d8d02ead9fb9f3b82d8e
Brian Lovin <brianlovin@github.com>
2019-01-16 17:10:54 -0800

    Copy tweaks

--- config/geotagging.js
+++ config/geotagging.js
@@ -1,9 +1,9 @@
 export default {
   id: 'geotagging',
-  title: 'Turn OFF Geotagging for your media',
-  description: `Geotagging is the process of adding geographical identification to a media files (photos, videos, etc.). Anyone who has access to your media files (for example, in Instagram or Twitter) can read this data and find out information that you wouldn’t want to disclose.
+  title: 'Review and remove metadata attached to photos you share',
+  description: `Geotagging is the process of adding geographical identification to media files (photos and videos, for example). Anyone who has access to these tagged media files (for example, on Instagram or Twitter) can read this data and learn where the photo was taken.
 
-  \n\nEnsure that your camera does not tag your photos and videos with the current Geo Position.
+  \n\nYou should understand how location metadata is attached to your media and take steps to ensure you are not uploading sensitive information with your files.
   `,
   resources: [
     {
@@ -11,25 +11,25 @@ export default {
       url: 'https://en.wikipedia.org/wiki/Geotagging',
     },
     {
-      name: 'What is Geotag Security and How it Helps to Protect Your Family',
+      name: 'What is geotag security and how it helps to protect your family',
       url: 'https://www.geotag-security.com/',
     },
     {
-      name: 'Web Photos That Reveal Secrets, Like Where You Live',
+      name: 'Web photos that reveal secrets, like where you live',
       url:
         'https://www.nytimes.com/2010/08/12/technology/personaltech/12basics.html',
     },
     {
-      name: 'How to Avoid the Potential Risks of Geotagging',
+      name: 'How to avoid the potential risks of geotagging',
       url: 'https://www.wikihow.com/Avoid-the-Potential-Risks-of-Geotagging',
     },
     {
-      name: 'How to Turn OFF Geotagging For Photos On iPhone and iPad',
+      name: 'How to turn off geotagging for photos on iPhone and iPad',
       url:
         'https://www.techbout.com/turn-off-geotagging-for-photos-iphone-ipad-8738/',
     },
     {
-      name: 'How To Turn Off Geotagging On Android Devices',
+      name: 'How To turn off geotagging on Android devices',
       url: 'https://www.technobezz.com/turn-off-geotagging-android-devices/',
     },
   ],
