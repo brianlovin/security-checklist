1f612349f85cc91abecc6a8c41a1889a8e7e8f28
Brian Lovin <brianlovin@github.com>
2019-01-22 10:49:10 -0800

    Reorder

--- config/passwordManager.js
+++ config/passwordManager.js
@@ -38,15 +38,6 @@ export default {
       },
     },
     {
-      name: 'iCloud Keychain',
-      image: '/static/img/icloud.png',
-      url: 'https://www.icloud.com/',
-      sources: {
-        macos: 'https://support.apple.com/en-us/HT204085',
-        ios: 'https://support.apple.com/en-us/HT204085'
-      },
-    },
-    {
       name: 'Dashlane',
       image: '/static/img/dashlane.jpg',
       url: 'https://www.dashlane.com/',
@@ -86,6 +77,15 @@ export default {
         macos: 'https://www.avast.com/passwords#mac',
         ios: 'https://www.avast.com/passwords#ios',
         android: 'https://www.avast.com/passwords#android',
+      },
+    },
+    {
+      name: 'iCloud Keychain',
+      image: '/static/img/icloud.png',
+      url: 'https://www.icloud.com/',
+      sources: {
+        macos: 'https://support.apple.com/en-us/HT204085',
+        ios: 'https://support.apple.com/en-us/HT204085'
       },
     },
   ],
