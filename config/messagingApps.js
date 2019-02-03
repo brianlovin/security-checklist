c2e7f8ee56ee4dba8994c2fcaee11c44c9b73744
Joachim <joachim.robert@protonmail.com>
2019-02-03 13:26:07 +0100

    ➕ Wire & Threema, ➖ iMessage & WhatsApp

--- config/messagingApps.js
+++ config/messagingApps.js
@@ -5,27 +5,6 @@ export default {
   `,
   apps: [
     {
-      name: 'iMessage',
-      image: '/static/img/imessage.png',
-      url: 'https://support.apple.com/explore/messages',
-      sources: {
-        macos: 'https://support.apple.com/explore/messages',
-        ios: 'https://support.apple.com/explore/messages',
-      },
-    },
-    {
-      name: 'WhatsApp',
-      image: '/static/img/whatsapp.jpg',
-      url: 'https://www.whatsapp.com/',
-      sources: {
-        windows: 'https://www.whatsapp.com/download',
-        macos: 'https://www.whatsapp.com/download',
-        ios:
-          'http://itunes.apple.com/us/app/whatsapp-messenger/id310633997?mt=8',
-        android: 'https://play.google.com/store/apps/details?id=com.whatsapp',
-      },
-    },
-    {
       name: 'Signal',
       image: '/static/img/signal.jpg',
       url: 'https://www.signal.org/',
@@ -37,6 +16,31 @@ export default {
         android:
           'https://play.google.com/store/apps/details?id=org.thoughtcrime.securesms&referrer=utm_source%3DOWS%26utm_medium%3DWeb%26utm_campaign%3DNav',
         linux: 'https://www.signal.org/download/',
+      },
+    },
+    {
+      name: 'Wire',
+      image: '/static/img/wire.jpg',
+      url: 'https://wire.com/en/',
+      sources: {
+        windows: 'https://wire.com/en/download/',
+        macos: 'https://wire.com/en/download/',
+        ios:
+          'https://itunes.apple.com/app/wire/id930944768?mt=8',
+        android:
+          'https://play.google.com/store/apps/details?id=com.wire',
+        linux: 'https://wire.com/en/download/',
+      },
+    },
+    {
+      name: 'Threema',
+      image: '/static/img/threema.jpg',
+      url: 'https://threema.ch/en/',
+      sources: {
+        ios:
+          'https://itunes.apple.com/de/app/threema/id578665578?mt=8&uo=4&at=10lJMu',
+        android:
+          'https://play.google.com/store/apps/details?id=ch.threema.app',
       },
     },
   ],
@@ -56,14 +60,14 @@ export default {
     },
     {
       name:
-        'Why you need a better handle on the WhatsApp, Signal and Telegram apps',
+        'Secure Messaging Apps Comparison',
       url:
-        'https://www.cnet.com/news/you-might-not-really-understand-how-encrypted-messaging-apps-work/',
+        'https://www.securemessagingapps.com/',
     },
     {
-      name: 'The best and worst encrypted messaging apps',
+      name: 'Secure Messaging? More Like A Secure Mess.',
       url:
-        'https://gizmodo.com/the-best-and-worst-encrypted-messaging-apps-1782424449',
+        'https://www.eff.org/deeplinks/2018/03/secure-messaging-more-secure-mess',
     },
   ],
 };
