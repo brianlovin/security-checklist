1d886966030d8e0c850788af237908b480edb947
Joachim Robert <joachim.robert@protonmail.com>
2019-02-08 22:10:58 +0100

    Update Apps component, functions as a function again, not as a Class

--- components/ChecklistItem/Apps.js
+++ components/ChecklistItem/Apps.js
@@ -1,6 +1,6 @@
 // @flow
 // $FlowIssue
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { AppsContainer, SectionHeading, ExpandContainer, ExpandContent } from './style';
 import { Button } from '../Button';
 import type { ChecklistResource } from '../../types';
@@ -11,79 +11,64 @@ type Props = {
   handleAppsExpand: Function,
 };
 
-type State = {
-  overflowExpanded: boolean,
-  contentHeight: number,
-};
+export const Apps = ({ resource, handleAppsExpand }: Props) => {
+  const [overflowExpanded, setOverflowExpanded] = useState(false);
+  const [contentHeight, setcontentHeight] = useState(2000);
 
-export class Apps extends React.Component<Props, State> {
-  state = { overflowExpanded: false, contentHeight: 2000, };
-  expandContentContainer: { current: null | HTMLDivElement }
+  const expandContentContainer = useRef(null);
 
-  constructor(props: Props) {
-    super(props)
-    if (!this.props.resource.apps) return null;
-    
-    this.expandContentContainer = React.createRef();
-  }
-
-  handleExpand = () => {
+  function handleExpand() {
     let expandContentHeight =
-      this.expandContentContainer.current
-        ? this.expandContentContainer.current.scrollHeight
-        : this.state.contentHeight;
+      expandContentContainer.current
+        ? expandContentContainer.current.scrollHeight
+        : contentHeight;
 
-    this.setState({
-      contentHeight: expandContentHeight,
-      overflowExpanded: !this.state.overflowExpanded,
-    })
+      setcontentHeight(expandContentHeight)
+      setOverflowExpanded(!overflowExpanded)
 
-    this.props.handleAppsExpand(this.state.overflowExpanded ? -expandContentHeight : expandContentHeight);
+    handleAppsExpand(overflowExpanded ? -expandContentHeight : expandContentHeight);
   }
 
-  render() {
-    let appList = this.props.resource.apps;
-    let overflowAppList;
-    if (appList && appList.length > 3) {
-      overflowAppList = appList.slice(3, appList.length);
-      appList = appList.slice(0, 3);
-    }
+  if (!resource.apps) return null;
 
-    const { overflowExpanded, contentHeight } = this.state;
-    const { resource } = this.props;
-
-    return (
-      <AppsContainer overflowExpanded={overflowExpanded}>
-        <SectionHeading>Apps</SectionHeading>
-        {appList && appList.map(app => (
-          <AppRow key={app.name} app={app} />
-        ))}
-
-        {overflowAppList && (
-          <React.Fragment>
-            <ExpandContent
-              id={`apps_${resource.id}`}
-              role="region"
-              tabindex="-1"
-              ref={this.expandContentContainer}
-              style={{ '--maxHeight': `${contentHeight}px` }}
-              aria-hidden={!overflowExpanded}
-            >
-              {overflowAppList.map(app => <AppRow key={app.name} app={app} />)}
-            </ExpandContent>
-          
-            <ExpandContainer
-              onClick={this.handleExpand}
-              role="button"
-              aria-expanded={overflowExpanded}
-              aria-controls={`apps_${resource.id}`}
-            >
-              <Button type="button">Show {overflowExpanded ? "less" : "more"} choices</Button>
-            </ExpandContainer>
-          </React.Fragment>
-        )}
-      </AppsContainer>
-    );
+  let appList = resource.apps;
+  let overflowAppList;
+  if (appList && appList.length > 3) {
+    overflowAppList = appList.slice(3, appList.length);
+    appList = appList.slice(0, 3);
   }
+
+  return (
+    <AppsContainer overflowExpanded={overflowExpanded}>
+      <SectionHeading>Apps</SectionHeading>
+      {appList && appList.map(app => (
+        <AppRow key={app.name} app={app} />
+      ))}
+
+      {overflowAppList && (
+        <React.Fragment>
+          <ExpandContent
+            id={`apps_${resource.id}`}
+            role="region"
+            tabindex="-1"
+            ref={expandContentContainer}
+            style={{ '--maxHeight': `${contentHeight}px` }}
+            aria-hidden={!overflowExpanded}
+          >
+            {overflowAppList.map(app => <AppRow key={app.name} app={app} />)}
+          </ExpandContent>
+        
+          <ExpandContainer
+            onClick={handleExpand}
+            role="button"
+            aria-expanded={overflowExpanded}
+            aria-controls={`apps_${resource.id}`}
+          >
+            <Button type="button">Show {overflowExpanded ? "less" : "more"} choices</Button>
+          </ExpandContainer>
+        </React.Fragment>
+      )}
+    </AppsContainer>
+  );
 }
 
